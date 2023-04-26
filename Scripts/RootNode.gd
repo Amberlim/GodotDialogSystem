bd25f8d7f566eadec4354fb7a51708817366b147
Holy Crusader <holy.tanukis@gmail.com>
2023-04-26 03:22:46 +0200

    Update RootNode with a _to_dict() function

--- Scripts/RootNode.gd
+++ Scripts/RootNode.gd
@@ -8,3 +8,23 @@ var node_type = "Root"
 func _ready():
 	title = node_type + "Node (" + id + ")"
 
+
+func _to_dict() -> Dictionary:
+	return {
+		"$type": "NodeRoot",
+		"ID": id,
+		"NextID": get_next_id(),
+		"Conditions": [],
+		"Actions": [],
+		"Flags": [],
+		"CustomProperties": []
+	}
+
+
+func get_next_id():
+	var parent: GraphEdit = get_parent()
+	
+	for connection in parent.get_connection_list():
+		if connection.get("from") == "RootNode":
+			var to = parent.get_node_or_null(NodePath(connection.get("to")))
+			return to.id
