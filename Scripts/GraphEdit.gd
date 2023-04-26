db4a57c17af533bdc3c742c409570d96967e51ec
Holy Crusader <holy.tanukis@gmail.com>
2023-04-26 12:58:39 +0200

    Mistake

--- Scripts/GraphEdit.gd
+++ Scripts/GraphEdit.gd
@@ -16,7 +16,7 @@ func get_all_connections_from_slot(from_node: StringName, from_port: int):
 	var connections = []
 	
 	for connection in get_connection_list():
-		if connection.get("from") == from_node:
+		if connection.get("from") == from_node and connection.get("from_port") == from_port:
 			var to = get_node_or_null(NodePath(connection.get("to")))
 			connections.append(to)
 
