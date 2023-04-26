7c9de2932187a92040e1c0d469fb72f1815e8d05
Holy Crusader <holy.tanukis@gmail.com>
2023-04-26 03:56:01 +0200

    _to_dict() function for DiceRoll node

--- Scripts/DiceRoll.gd
+++ Scripts/DiceRoll.gd
@@ -1,9 +1,25 @@
 extends GraphNode
 
 
+@onready var target_number = $VBoxContainer/HBoxContainer2/TargetNumber
+
 var node_type = "NodeDiceRoll"
 var id = UUID.v4()
 
-
 func _ready():
 	title = node_type + " (" + id + ")"
+
+
+func _to_dict() -> Dictionary:
+	return {
+		"$type": node_type,
+		"ID": id,
+		"Skill": "",
+		"Target": target_number.value,
+		"PassID": 0,
+		"FailID": 0,
+		"Conditions": [],
+		"Actions": [],
+		"Flags": [],
+		"CustomProperties": []
+	}
