44ff945f55de66f787c5cc2507f8d1087777c92a
Holy Crusader <holy.tanukis@gmail.com>
2023-06-20 23:04:05 +0200

    Config button work

--- Scripts/SidePanelNodes/SidePanelNodeDetails.gd
+++ Scripts/SidePanelNodes/SidePanelNodeDetails.gd
@@ -4,6 +4,7 @@ extends PanelContainer
 @onready var label_id = $MarginContainer/ScrollContainer/PanelContainer/HBoxContainer/LabelID
 @onready var panel_container = $MarginContainer/ScrollContainer/PanelContainer
 @onready var graph_edit = $"../../GraphEdit"
+@onready var control_node = $"../../../.."
 
 @onready var root_node_panel_instance = preload("res://Objects/SidePanelNodes/RootNodePanel.tscn")
 @onready var sentence_node_panel_instance = preload("res://Objects/SidePanelNodes/SentenceNodePanel.tscn")
@@ -16,13 +17,16 @@ var current_panel = null
 func _ready():
 	hide()
 
-
-func _on_graph_edit_node_selected(node):
+func clear_current_panel():
 	graph_edit.speakers
 	if current_panel:
 		current_panel.queue_free()
 		current_panel = null
-		
+
+
+func _on_graph_edit_node_selected(node):
+	clear_current_panel()
+	
 	if node.node_type == "NodeEndPath":
 		return
 	
@@ -49,3 +53,20 @@ func _on_graph_edit_node_selected(node):
 
 func _on_texture_button_pressed():
 	hide()
+
+
+func _on_config_pressed():
+	clear_current_panel()
+	
+	var root_node = control_node.root_node_ref
+
+	var new_panel = root_node_panel_instance.instantiate()
+	new_panel.graph_node = root_node
+	current_panel = new_panel
+	
+	panel_container.add_child(new_panel)
+	new_panel._from_dict(root_node._to_dict())
+	
+	label_id.text = root_node.id
+	
+	show()
